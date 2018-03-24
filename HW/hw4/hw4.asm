28e8c09ab483fbe11488418fb0b8a99da4115a0a
Jacob Beneski <jacobbeneski@gmail.com>
2018-03-23 20:49:42 -0700

    hw4

--- HW/hw4/hw4.asm
+++ HW/hw4/hw4.asm
@@ -4,12 +4,18 @@ SECTION .data
 	array_len db 15
 	newline db 10
 
-	msg db "Test"
-	msg_len equ $-msg
+	msg_all db "Printing all: "
+	msg_all_len equ $-msg_all
+
+	msg_odd db "Printing odd: "
+	msg_odd_len equ $-msg_all
+
+	msg_even db "Printing even: "
+	msg_even_len equ $-msg_all
 
 
 SECTION .bss
-	outstring resw 4
+	num resd 4
 
 
 SECTION .text
@@ -17,20 +23,46 @@ SECTION .text
 
 	_start:
 
+	push msg_all			;Push The Output Message
+	push msg_all_len		;Push the length
+	call writeString		;Print the message
+
 
 	mov eax, array			;Store Array
-	mov ebx, array_len		;Store Array_Len
-	mov ecx, outstring
+	mov ecx, array_len		;Store Array_Len
+	mov edx, 0			;Counter
 	call printAll			;Call PrintAll Subroutine
 
-	push outstring
-	push 30
-	call writeString	
-	
-	;mov eax, [array]
-	;mov ebx, array_len
-	;mov ecx, outstring
-	;call printEven
+	push newline			;Push a newline
+	push 1				;Length
+	call writeString		;Print a newline
+
+	push msg_even
+	push msg_even_len
+	call writeString
+
+	mov eax, array			;Store Array
+	mov ecx, array_len		;Store Array_Len
+	mov edx, 0
+	call printAll			;Call PrintAll Subroutine
+
+	push newline
+	push 1
+	call writeString
+
+	push msg_odd
+	push msg_odd_len
+	call writeString
+
+
+	mov eax, array			;Store Array
+	mov ecx, array_len		;Store Array_Len
+	mov edx, 0
+	call printAll			;Call PrintAll Subroutine
+
+	push newline
+	push 1
+	call writeString
 
 	
 	call exit
@@ -39,13 +71,17 @@ SECTION .text
 
 printAll:
 	
-	mov bl, [eax]
-	mov [ecx], bl
-	inc eax
-	inc ecx
+	mov bl, [eax + edx]
+	add bl, '0'
+	mov [num], bl
+	push num
+	push dword 1
 
+	call writeString
 
-	dec byte [array_len]
+	inc edx
+
+	dec byte [ecx]
 	jnz printAll
 	
 
